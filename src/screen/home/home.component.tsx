2a3d5bfe44943741349df9f1eafb10b772879ca0
matteocavallo <mcavallo@soldo.com>
2021-06-26 12:59:54 +0200

    build config

--- src/screen/home/home.component.tsx
+++ src/screen/home/home.component.tsx
@@ -1,13 +1,5 @@
-import { Button, Col, Layout, Row, Space } from "antd";
-import { Content, Footer, Header } from "antd/lib/layout/layout";
-import Title from "antd/lib/typography/Title";
-
-import { FC, useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import { eventService } from "../../service/event.service";
-import { EventDTO } from "../../service/types";
 import { CartComponent } from "./cart/cart.component";
-import { EventsList } from "./component/eventsList/eventsList.component";
 import { Navbar } from "./component/navbar.component";
 import { EventDetailPage } from "./events/eventDetails/eventDetail.component";
 import {UserPage} from '../user/user.component';
